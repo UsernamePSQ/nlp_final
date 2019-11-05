353cfcbc70f16fb63824566ad8ef1abcecfd1654
SebastianGPedersen <Sebastian.g.pedersen@gmail.com>
2019-11-05 15:17:58 +0100

    plot_confusion_matrix had wrong y_true and y_pred

--- stat-nlp-book/assignments/2019/final_assignment/problem/Extra_files/modules/scaling.py
+++ stat-nlp-book/assignments/2019/final_assignment/problem/Extra_files/modules/scaling.py
@@ -2,7 +2,7 @@ import numpy as np
 from copy import deepcopy
 from collections import Counter
 
-def upscale(data_m_XY, scale_factor = 5):
+def upscale(data_m_XY, scale_factor = 12):
     '''
     This function upscales synonym, hyponym and hyponym_reverted with scale_factor
     '''
