9c73db91cf2ee3f48379571d098a87550ef15417
psq <psq819@alumni.ku.dk>
2019-11-07 09:37:48 +0100

    weak model

--- stat-nlp-book/assignments/2019/final_assignment/problem/Extra_files/modules/DataSeq.py
+++ stat-nlp-book/assignments/2019/final_assignment/problem/Extra_files/modules/DataSeq.py
@@ -5,19 +5,26 @@ import os
 
 class trainable_emb(Sequence):
 
-    def __init__(self, train=True, up=10, hard_path=False):
+    def __init__(self, train=True, up=10, hard_path=False, UMLS=False):
         if hard_path:
             dir_ = '/home/mw/nlp/nlp_final/stat-nlp-book/assignments/2019/final_assignment/problem/Extra_files/resources/'
         else:
             dir_ = os.path.join(os.getcwd(), 'Extra_files/resources')
 
         if train:
-            if up == 10:
-                file = 'tf_train_emb_up10.pkl'
+            if not UMLS:
+                if up == 10:
+                    file = 'tf_train_emb_up10.pkl'
+                else:
+                    file = 'tf_train_emb.pkl'
             else:
-                file = 'tf_train_emb.pkl'
+                file = 'tf_train_UMLS.pkl'
+
         else:
-            file = 'tf_dev_emb.pkl'
+            if not UMLS:
+                file = 'tf_dev_emb.pkl'
+            else:
+                file = 'tf_dev_UMLS.pkl'
 
         with open(os.path.join(dir_, file), 'rb') as f:
             self.data = load(f)
@@ -27,4 +34,3 @@ class trainable_emb(Sequence):
 
     def __getitem__(self, item):
         return self.data[item]
-
